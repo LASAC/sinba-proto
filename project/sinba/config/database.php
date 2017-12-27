7a2e07eb67f5171b6996aa838dea92ed2808ca44
Geraldo B. Landre <geraldo.landre@aequilibrium.ca>
2017-12-26 16:36:12 -0800

    SQLite Database Set up para desenvolvimento local

--- project/sinba/config/database.php
+++ project/sinba/config/database.php
@@ -48,7 +48,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => database_path(env('DB_DATABASE')),
+            'database' => database_path('database.sqlite'),
             'prefix' => '',
         ],
 
